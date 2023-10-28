939d7384a5cb78131075dea50fcfb26ce9714f50
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-10-28 20:51:58 +0530

    Bill added

--- bluetooth_printer_app/lib/main.dart
+++ bluetooth_printer_app/lib/main.dart
@@ -1,125 +1,265 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
+import 'package:intl/intl.dart';
+import 'package:bluetooth_print/bluetooth_print.dart';
+import 'package:bluetooth_print/bluetooth_print_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  BluetoothPrint bluetoothPrint = BluetoothPrint.instance;
 
-  // This widget is the root of your application.
+  bool _connected = false;
+  BluetoothDevice? _device;
+  String tips = 'no device connect';
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => initBluetooth());
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initBluetooth() async {
+    bluetoothPrint.startScan(timeout: Duration(seconds: 4));
+
+    bool isConnected=await bluetoothPrint.isConnected??false;
+
+    bluetoothPrint.state.listen((state) {
+      print('******************* cur device status: $state');
+
+      switch (state) {
+        case BluetoothPrint.CONNECTED:
+          setState(() {
+            _connected = true;
+            tips = 'connect success';
+          });
+          break;
+        case BluetoothPrint.DISCONNECTED:
+          setState(() {
+            _connected = false;
+            tips = 'disconnect success';
+          });
+          break;
+        default:
+          break;
+      }
+    });
+
+    if (!mounted) return;
+
+    if(isConnected) {
+      setState(() {
+        _connected=true;
+      });
+    }
+  }
+
+  String generatedReceiptId() {
+    final random = Random();
+    int min = 0; // Smallest 5-digit number
+    int max = 99999; // Largest 5-digit number
+    int randomNumber = min + random.nextInt(max - min + 1);
+    return randomNumber.toString().padLeft(5, '0');
+  }
+
+  String getCurrentDateTime() {
+    final now = DateTime.now();
+    final formattedDate = DateFormat('yyyy-MM-dd HH:mm:ss').format(now);
+    return formattedDate;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('BluetoothPrint example app'),
+        ),
+        body: RefreshIndicator(
+          onRefresh: () =>
+              bluetoothPrint.startScan(timeout: Duration(seconds: 4)),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      child: Text(tips),
+                    ),
+                  ],
+                ),
+                Divider(),
+                StreamBuilder<List<BluetoothDevice>>(
+                  stream: bluetoothPrint.scanResults,
+                  initialData: [],
+                  builder: (c, snapshot) => Column(
+                    children: snapshot.data!.map((d) => ListTile(
+                      title: Text(d.name??''),
+                      subtitle: Text(d.address??''),
+                      onTap: () async {
+                        setState(() {
+                          _device = d;
+                        });
+                      },
+                      trailing: _device!=null && _device!.address == d.address?Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ):null,
+                    )).toList(),
+                  ),
+                ),
+                Divider(),
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 5, 20, 10),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          OutlinedButton(
+                            child: Text('connect'),
+                            onPressed:  _connected?null:() async {
+                              if(_device!=null && _device!.address !=null){
+                                setState(() {
+                                  tips = 'connecting...';
+                                });
+                                await bluetoothPrint.connect(_device!);
+                              }else{
+                                setState(() {
+                                  tips = 'please select device';
+                                });
+                                print('please select device');
+                              }
+                            },
+                          ),
+                          SizedBox(width: 10.0),
+                          OutlinedButton(
+                            child: Text('disconnect'),
+                            onPressed:  _connected?() async {
+                              setState(() {
+                                tips = 'disconnecting...';
+                              });
+                              await bluetoothPrint.disconnect();
+                            }:null,
+                          ),
+                        ],
+                      ),
+                      Divider(),
+                      OutlinedButton(
+                        child: Text('print receipt(esc)'),
+                        onPressed:  _connected?() async {
+                          print(generatedReceiptId());
+                          Map<String, dynamic> config = Map();
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+                          List<LineText> list = [];
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: '--------------------------------', weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
+                          // // Add the image before the 'Taxperts' content
+                          // ByteData imageByteData = await rootBundle.load("images/BWCBS.png");
+                          // List<int> imageBytes = imageByteData.buffer.asUint8List(imageByteData.offsetInBytes, imageByteData.lengthInBytes);
+                          // String base64Image = base64Encode(imageBytes);
+                          // list.add(LineText(type: LineText.TYPE_IMAGE, content: base64Image, align: LineText.ALIGN_CENTER, linefeed: 1));
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'Mega Mart', weight: 1, align: LineText.ALIGN_CENTER, fontZoom: 2, linefeed: 1));
+                          list.add(LineText(linefeed: 1));
 
-  final String title;
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: '3rd Floor,No.101,Olcott Mawatha,Colombo 11', weight: 1, align: LineText.ALIGN_CENTER, linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'Tel: 011-2424922 | 077-7145347', weight: 1, align: LineText.ALIGN_CENTER, linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: '--------------------------------', weight: 1, align: LineText.ALIGN_CENTER, linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'Receipt No:', weight: 1, align: LineText.ALIGN_LEFT, x: 0, relativeX: 0, linefeed: 0));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: generatedReceiptId(), weight: 1, align: LineText.ALIGN_LEFT, x: 140, relativeX: 0, linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'Cashier:', weight: 1, align: LineText.ALIGN_LEFT, x: 0, relativeX: 0, linefeed: 0));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: "ShahiruE", weight: 1, align: LineText.ALIGN_LEFT, x: 110, relativeX: 0, linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: getCurrentDateTime(), weight: 1, align: LineText.ALIGN_LEFT, x: 0, relativeX: 0, linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'Payment Type:', weight: 1, align: LineText.ALIGN_LEFT, x: 0, relativeX: 0, linefeed: 0));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: "Cash", weight: 1, align: LineText.ALIGN_LEFT, x: 165, relativeX: 0, linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: '--------------------------------', weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'Item Price', weight: 1, align: LineText.ALIGN_LEFT, x: 0, relativeX: 0, linefeed: 0));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'Qty', weight: 1, align: LineText.ALIGN_LEFT, x: 155, relativeX: 0, linefeed: 0));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: "Total(Rs)", weight: 1, align: LineText.ALIGN_LEFT, x: 255, relativeX: 0, linefeed: 1));
+                          list.add(LineText(linefeed: 1));
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: "1. 3pc Vacuum Pack", weight: 1, align: LineText.ALIGN_LEFT, x: 0, relativeX: 0, linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: '30.00', weight: 1, align: LineText.ALIGN_LEFT, x: 5, relativeX: 0, linefeed: 0));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: '8', weight: 1, align: LineText.ALIGN_LEFT, x: 160, relativeX: 0, linefeed: 0));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: "240.00", weight: 1, align: LineText.ALIGN_LEFT, x: 260, relativeX: 0, linefeed: 1));
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: '--------------------------------', weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
+                          list.add(LineText(linefeed: 1));
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+                          await bluetoothPrint.printReceipt(config, list);
+                        }:null,
+                      ),
+                      OutlinedButton(
+                        child: Text('print My Details'),
+                        onPressed:  _connected?() async {
+                          Map<String, dynamic> config = Map();
+
+                          List<LineText> list = [];
+
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: '**********************************************', weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'My Deatails', weight: 1, align: LineText.ALIGN_CENTER, fontZoom: 2, linefeed: 1));
+                          list.add(LineText(linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'Email: ', weight: 1, align: LineText.ALIGN_LEFT, x: 0,relativeX: 0, linefeed: 0));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: '  Address', weight: 1, align: LineText.ALIGN_LEFT, x: 350, relativeX: 0, linefeed: 0));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'Phone', weight: 1, align: LineText.ALIGN_LEFT, x: 500, relativeX: 0, linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: '**********************************************', weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
+                          list.add(LineText(linefeed: 1));
+
+                          ByteData data = await rootBundle.load("images/BWCBS.png");
+                          List<int> imageBytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+                          String base64Image = base64Encode(imageBytes);
+                          // list.add(LineText(type: LineText.TYPE_IMAGE, content: base64Image, align: LineText.ALIGN_CENTER, linefeed: 1));
+
+                          await bluetoothPrint.printReceipt(config, list);
+                        }:null,
+                      ),
+
+
+
+                      OutlinedButton(
+                        child: Text('print selftest'),
+                        onPressed:  _connected?() async {
+                          await bluetoothPrint.printTest();
+                        }:null,
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
+        ),
+        floatingActionButton: StreamBuilder<bool>(
+          stream: bluetoothPrint.isScanning,
+          initialData: false,
+          builder: (c, snapshot) {
+            if (snapshot.data == true) {
+              return FloatingActionButton(
+                child: Icon(Icons.stop),
+                onPressed: () => bluetoothPrint.stopScan(),
+                backgroundColor: Colors.red,
+              );
+            } else {
+              return FloatingActionButton(
+                  child: Icon(Icons.search),
+                  onPressed: () => bluetoothPrint.startScan(timeout: Duration(seconds: 4)));
+            }
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
